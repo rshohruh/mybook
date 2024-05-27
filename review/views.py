1bb6025598a3956414381d1ebcf8106dde8496a5
rshohruh <rshohrux16@gmail.com>
2024-05-27 18:42:19 +0500

    mvt-beginner branch

--- review/views.py
+++ review/views.py
@@ -1,3 +1,53 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
-# Create your views here.
+from .models import Book
+from .utils import average_rating
+
+
+def index(request):
+    return render(request, "base.html")
+
+
+# def book_search(request):
+#     search_text = request.GET.get("search", "")
+#     return render(request, "review/search-results.html", {"search_text": search_text})
+#
+
+def book_list(request):
+    books = Book.objects.all()
+    # print(books)
+    books_with_reviews = []
+    for book in books:
+        reviews = book.review_set.all()
+        if reviews:
+            book_rating = average_rating([review.rating for review in reviews])
+            number_of_reviews = len(reviews)
+        else:
+            book_rating = None
+            number_of_reviews = 0
+        books_with_reviews.append({"book": book, "book_rating": book_rating, "number_of_reviews": number_of_reviews})
+
+    context = {
+        "book_list": books_with_reviews
+    }
+    # print(context)
+    return render(request, "review/book_list.html", context)
+
+#
+# def book_detail(request, pk):
+#     book = get_object_or_404(Book, pk=pk)
+#     reviews = book.review_set.all()
+#     if reviews:
+#         book_rating = average_rating([review.rating for review in reviews])
+#         context = {
+#             "book": book,
+#             "book_rating": book_rating,
+#             "reviews": reviews
+#         }
+#     else:
+#         context = {
+#             "book": book,
+#             "book_rating": None,
+#             "reviews": None
+#         }
+#     return render(request, "review/book_detail.html", context)
